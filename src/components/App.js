1b60dbc90f0d2cc4c523517613985cd30b04f9b4
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-16 18:13:21 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
+import React, { useState, useEffect } from 'react';
 
-import React from "react";
-import './../styles/App.css';
+const images = [
+  "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem/image/MQ6-0-image-20220419101504/01-mercedes-benz-the-new-eqs-suv-x296-2021-1080x1350.jpeg",
+  "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_546277676/image/MQ6-0-image-20220819111550/02-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg",
+  "https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_354875624/image/MQ6-0-image-20220419101505/03-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg",
+];
 
-const App = () => {
+function App() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // Change the image every 5 seconds
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    // Clean up the timer when the component is unmounted
+    return () => clearInterval(timer);
+  }, []); // Empty dependency array means this effect runs once on mount
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Mercedes Slideshow</h1>
+      <img
+        src={images[currentImageIndex]}
+        alt={`Mercedes Slideshow ${currentImageIndex + 1}`}
+        style={{ maxWidth: '100%', maxHeight: '100%' }}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
